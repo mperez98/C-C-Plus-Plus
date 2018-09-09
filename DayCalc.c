e0080abc2a110ac2636cd66147f37e6a43c6f492
mperez98 <33238506+mperez98@users.noreply.github.com>
2018-09-09 12:34:51 -0500

    PractProb1
    https://www.hackerrank.com/challenges/day-of-the-programmer/problem

---
+++ DayCalc.c
@@ -0,0 +1,27 @@
+//
+//  DayCalc.c
+//  PracticeProblems
+//
+//  Created by Mathew Perez on 9/9/18.
+//  Copyright © 2018 Mathew Perez. All rights reserved.
+//
+
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+char * dayOfProgrammer(int year) {
+}
+
+
