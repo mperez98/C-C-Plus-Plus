c34e529419efa92f2d617d4c0f7a781c8917bf30
mperez98 <33238506+mperez98@users.noreply.github.com>
2018-09-09 12:35:34 -0500

    Update DayCalc.c

--- DayCalc.c
+++ DayCalc.c
@@ -21,7 +21,113 @@ char* readline();
 char* ltrim(char*);
 char* rtrim(char*);
 
-char * dayOfProgrammer(int year) {
+char* dayOfProgrammer(int year) {
+    if (year == 1918) {
+        return "26.09.1918";
+    } else if (((year <= 1917) && (year%4 == 0)) || ((year > 1918) && (year%400 == 0 || ((year%4 == 0)&&(year%100 != 0))))){
+        int yearLen = floor(log10(abs(year))) + 1;
+        char buffer[yearLen];
+        itoa(year, buffer, 10);
+        return "12.09." + year;
+    } else {
+        int yearLen = floor(log10(abs(year))) + 1;
+        char buffer[yearLen];
+        itoa(year, buffer, 10);
+        return "13.09." + year;
+    }
+    
 }
 
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+    
+    char* year_endptr;
+    char* year_str = ltrim(rtrim(readline()));
+    int year = strtol(year_str, &year_endptr, 10);
+    
+    if (year_endptr == year_str || *year_endptr != '\0') { exit(EXIT_FAILURE); }
+    
+    char* result = dayOfProgrammer(year);
+    
+    fprintf(fptr, "%s\n", result);
+    
+    fclose(fptr);
+    
+    return 0;
+}
 
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+    
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+        
+        if (!line) {
+            break;
+        }
+        
+        data_length += strlen(cursor);
+        
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+        alloc_length <<= 1;
+        data = realloc(data, alloc_length);
+        
+        if (!data) {
+            data = '\0';
+            break;
+        }
+    }
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+        
+        data = realloc(data, data_length);
+        
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+        
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+    if (!*str) {
+        return str;
+    }
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+    if (!*str) {
+        return str;
+    }
+    char* end = str + strlen(str) - 1;
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+    *(end + 1) = '\0';
+    
+    return str;
+}
